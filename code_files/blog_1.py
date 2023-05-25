fc650e753d36a14da850e41dfa1ef001e14b59b1
abhishekmamdapure <mamdapureabhishek@gmail.com>
2023-05-25 10:42:52 +0530

    added folders

---
+++ code_files/blog_1.py
@@ -0,0 +1,17 @@
+
+from wasabi import Printer
+msg = Printer()
+print('\n\n\n\n')
+msg.text("Hello world!")
+
+print('\n\n\n\n')
+
+
+# from time import sleep
+# from wasabi import msg, Printer
+
+# if __name__ == '__main__':
+#     msg = Printer()
+#     with msg.loading("Loading.."):
+#         sleep(10)
+#     msg.good("done")
